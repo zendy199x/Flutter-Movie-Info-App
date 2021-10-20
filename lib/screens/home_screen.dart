34a902c5f816d4b4d2e3e459bcabef23ebb4299e
Zendy <nguyenduc.051194@gmail.com>
2021-10-20 23:54:04 +0700

    App Bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:movie_info/constants.dart';
+import 'package:movie_info/screens/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: buildAppbar(),
+      body: const Body(),
+    );
+  }
+
+  AppBar buildAppbar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        padding: const EdgeInsets.only(left: kDefaultPadding),
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding,
+          ),
+          onPressed: () {},
+          icon: SvgPicture.asset("assets/icons/search.svg"),
+        ),
+      ],
+    );
   }
 }
